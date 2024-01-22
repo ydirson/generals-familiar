56f5c287258f3fbdab59a2bef080fad5796dd1fb
Yann Dirson <ydirson@free.fr>
2024-01-22 19:29:29 +0100

    WIP Initial prototype with Leptos and Leptonic
    - fetches and deserializes a single army
    - present a single member from army data (name)
    
    FIXME:
    - missing theme?
    - quite some String copying already
    - Rc<Army> is there to avoid copying the struct, but is that a proper
      way of dealing with the problem?
    - find a pattern to unpack an `Option<Rc<_>>`

---
+++ src/main.rs
@@ -0,0 +1,62 @@
+mod opr_data;
+
+use gloo_net::http::Request;
+use leptonic::prelude as ltn;
+use leptos::*;
+use opr_data as opr;
+use std::rc::Rc;
+
+const APP_NAME: &str = "General's Familiar";
+const PLAYER_NAMES: [&str; 2] = ["Player 1", "Player 2"];
+const ARMY_IDS: [&str; 2] = ["ybjR2-7kHUNY", "VV8Zy0GIfOUX"];
+
+fn main() {
+    mount_to_body(|| view! { <App/> })
+}
+
+#[component]
+fn App() -> impl IntoView {
+    let (army_id, set_army_id) = create_signal(ARMY_IDS[0].to_string());
+    view! {
+        <ltn::AppBar>
+            <h1>{APP_NAME}</h1>
+        </ltn::AppBar>
+        <ArmyList army_id player_name=PLAYER_NAMES[0].to_string() />
+    }
+}
+
+#[component]
+pub fn ArmyList(player_name: String, army_id: ReadSignal<String>) -> impl IntoView {
+    let army_data = create_resource(
+        move || army_id.get(),
+        |army_id_value| async move { load_data(&army_id_value).await });
+
+    view! {
+        <h2>{player_name} " - "
+        {move || army_data.with(
+            |army_data| match army_data {
+                None => view! { "Loading..." }.into_view(),
+                Some(army) => {
+                    let opr::Army{ref game_system, ref name, ..} = **army;
+                    view! {
+                        {game_system.to_uppercase()}
+                        " - "
+                        {name}
+                    }
+                }.into_view()
+            }
+        )}
+        </h2>
+    }
+}
+
+async fn load_data(army_id: &str) -> Rc<opr::Army> {
+    let army_url = format!("{}?id={army_id}", opr::GET_ARMY_BASE_URL);
+    Request::get(army_url.as_str())
+        .send()
+        .await
+        .expect("should get an HTTP answer")
+        .json()
+        .await
+        .expect("should deserialize Army from JSON content")
+}
---
+++ src/opr_data.rs
@@ -0,0 +1,47 @@
+use serde::{Deserialize, Serialize};
+use serde_aux::field_attributes::deserialize_number_from_string;
+use std::rc::Rc;
+
+pub const GET_ARMY_BASE_URL: &str = "https://army-forge.onepagerules.com/api/tts";
+
+#[derive(Clone, PartialEq, Debug, Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Army {
+    pub id: String,
+    pub name: String,
+    pub game_system: String,
+    pub points: usize,
+    pub points_limit: usize,
+    pub units: Vec<Rc<Unit>>,
+}
+
+#[derive(PartialEq, Debug, Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Unit {
+    pub id: String,
+    pub name: String,
+    pub size: usize,
+    pub quality: usize,
+    pub defense: usize,
+    pub special_rules: Vec<Rc<SpecialRule>>,
+    pub equipment: Vec<Rc<Equipment>>,
+}
+
+#[derive(PartialEq, Debug, Deserialize, Serialize)]
+pub struct SpecialRule {
+    pub name: String,
+    #[serde(default)]
+    pub rating: String,
+}
+
+#[derive(PartialEq, Debug, Deserialize, Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Equipment {
+    pub id: String,
+    pub name: String,
+    pub range: usize,
+    #[serde(deserialize_with = "deserialize_number_from_string")]
+    pub attacks: usize,
+    pub count: usize,
+    pub special_rules: Vec<Rc<SpecialRule>>,
+}
