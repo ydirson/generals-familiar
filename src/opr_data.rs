c1e73f1c94e4a57fc42a3510d4c729383beebd9c
Yann Dirson <ydirson@free.fr>
2024-01-26 16:50:42 +0100

    fixup! Initial prototype with Leptos and Leptonic
    move all components out of generic OPR module

--- src/opr_data.rs
+++ src/opr_data.rs
@@ -1,85 +1,47 @@
-use gloo_net::http::Request;
-use leptos::*;
 use serde::{Deserialize, Serialize};
 use serde_aux::field_attributes::deserialize_number_from_string;
 use std::rc::Rc;
 
-const GET_ARMY_BASE_URL: &str = "https://army-forge.onepagerules.com/api/tts";
+pub const GET_ARMY_BASE_URL: &str = "https://army-forge.onepagerules.com/api/tts";
 
 #[derive(Clone, PartialEq, Debug, Deserialize, Serialize)]
 #[serde(rename_all = "camelCase")]
 pub struct Army {
-    id: String,
-    name: String,
-    game_system: String,
-    points: usize,
-    points_limit: usize,
-    units: Vec<Rc<Unit>>,
+    pub id: String,
+    pub name: String,
+    pub game_system: String,
+    pub points: usize,
+    pub points_limit: usize,
+    pub units: Vec<Rc<Unit>>,
 }
 
 #[derive(PartialEq, Debug, Deserialize, Serialize)]
 #[serde(rename_all = "camelCase")]
 pub struct Unit {
-    id: String,
-    name: String,
-    size: usize,
-    quality: usize,
-    defense: usize,
-    special_rules: Vec<Rc<SpecialRule>>,
-    equipment: Vec<Rc<Equipment>>,
+    pub id: String,
+    pub name: String,
+    pub size: usize,
+    pub quality: usize,
+    pub defense: usize,
+    pub special_rules: Vec<Rc<SpecialRule>>,
+    pub equipment: Vec<Rc<Equipment>>,
 }
 
 #[derive(PartialEq, Debug, Deserialize, Serialize)]
 pub struct SpecialRule {
-    name: String,
+    pub name: String,
     #[serde(default)]
-    rating: String,
+    pub rating: String,
 }
 
 #[derive(PartialEq, Debug, Deserialize, Serialize)]
 #[serde(rename_all = "camelCase")]
 pub struct Equipment {
-    id: String,
-    name: String,
-    range: usize,
+    pub id: String,
+    pub name: String,
+    pub range: usize,
     #[serde(deserialize_with = "deserialize_number_from_string")]
-    attacks: usize,
-    count: usize,
-    special_rules: Vec<Rc<SpecialRule>>,
-}
-
-async fn load_data(army_id: &str) -> Rc<Army> {
-    let army_url = format!("{GET_ARMY_BASE_URL}?id={army_id}");
-    Request::get(army_url.as_str())
-        .send()
-        .await
-        .expect("should get an HTTP answer")
-        .json()
-        .await
-        .expect("should deserialize Army from JSON content")
-}
-
-#[component]
-pub fn ArmyList(player_name: String, army_id: ReadSignal<String>) -> impl IntoView {
-    let army_data = create_resource(
-        move || army_id.get(),
-        |army_id_value| async move { load_data(&army_id_value).await });
-
-    view! {
-        <h2>{player_name} " - "
-        {move || army_data.with(
-            |army_data| match army_data {
-                None => view! { "Loading..." }.into_view(),
-                Some(army) => {
-                    let Army{ref game_system, ref name, ..} = **army;
-                    view! {
-                        {game_system.to_uppercase()}
-                        " - "
-                        {name}
-                    }
-                }.into_view()
-            }
-        )}
-        </h2>
-    }
+    pub attacks: usize,
+    pub count: usize,
+    pub special_rules: Vec<Rc<SpecialRule>>,
 }
