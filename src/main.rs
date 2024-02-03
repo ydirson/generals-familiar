ebbc9ea07d2801c7768c8d2ed5bb1c9a32dbfbf4
Lukas Potthast <privat@lukas-potthast.de>
2024-02-03 16:24:44 +0100

    migrate

--- src/main.rs
+++ src/main.rs
@@ -24,26 +24,36 @@ fn App() -> impl IntoView {
     let (unitsel1, set_unitsel1) = create_signal(None::<Rc<opr::Unit>>);
     view! {
         <ltn::Root default_theme=ltn::LeptonicTheme::default()>
-            <ltn::AppBar style="z-index: 1; background: var(--brand-color); color: white;">
-                <h1>{APP_NAME}</h1>
-                <ltn::ThemeToggle off=ltn::LeptonicTheme::Light on=ltn::LeptonicTheme::Dark/>
-            </ltn::AppBar>
-            <DetailsDrawer side=ltn::DrawerSide::Left unit_selection=unitsel0 />
-            <ltn::Stack spacing=ltn::Size::Em(0.5)>
-                <ltn::Stack orientation=ltn::StackOrientation::Horizontal
-                       spacing=ltn::Size::Em(1.0)
-                       style="margin-right: 1em">
-                    <ArmyList army_id=army_id0
-                              player_name=PLAYER_NAMES[0].to_string()
-                              select_unit=set_unitsel0
-                    />
-                    <ArmyList army_id=army_id1
-                              player_name=PLAYER_NAMES[1].to_string()
-                              select_unit=set_unitsel1
-                     />
+            <ltn::Box style="height: 100%;">
+                <ltn::AppBar style="z-index: 1; background: var(--brand-color); color: white;">
+                    <h1>{APP_NAME}</h1>
+                    <ltn::ThemeToggle off=ltn::LeptonicTheme::Light on=ltn::LeptonicTheme::Dark/>
+                </ltn::AppBar>
+                <DetailsDrawer side=ltn::DrawerSide::Left unit_selection=unitsel0 />
+
+
+                <h1>"General's Familiar - Apps from CI"</h1>
+                <ul>
+                  <li><a href="master/">"Branch master"</a></li>
+                  <li><a href="leptonic-0.5/">"Branch leptonic-0.5"</a></li>
+                </ul>
+
+                <ltn::Stack spacing=ltn::Size::Em(0.5)>
+                    <ltn::Stack orientation=ltn::StackOrientation::Horizontal
+                        spacing=ltn::Size::Em(1.0)
+                        style="margin-right: 1em">
+                        <ArmyList army_id=army_id0
+                                player_name=PLAYER_NAMES[0].to_string()
+                                select_unit=set_unitsel0
+                        />
+                        <ArmyList army_id=army_id1
+                                player_name=PLAYER_NAMES[1].to_string()
+                                select_unit=set_unitsel1
+                        />
+                    </ltn::Stack>
                 </ltn::Stack>
-            </ltn::Stack>
-            <DetailsDrawer side=ltn::DrawerSide::Right unit_selection=unitsel1 />
+                <DetailsDrawer side=ltn::DrawerSide::Right unit_selection=unitsel1 />
+            </ltn::Box>
         </ltn::Root>
     }
 }
@@ -107,12 +117,12 @@ fn UnitsList(units: Vec<Rc<opr::Unit>>,
     view! {
         <ltn::TableContainer>
             <ltn::Table bordered=true hoverable=true>
-                <ltn::Thead>
-                    <ltn::Tr>
-                        <ltn::Th>"Name"</ltn::Th>
-                    </ltn::Tr>
-                </ltn::Thead>
-                <ltn::Tbody>
+                <ltn::TableHeader>
+                    <ltn::TableRow>
+                        <ltn::TableHeaderCell>"Name"</ltn::TableHeaderCell>
+                    </ltn::TableRow>
+                </ltn::TableHeader>
+                <ltn::TableBody>
                     {move || {
                         units
                             .clone()
@@ -121,16 +131,16 @@ fn UnitsList(units: Vec<Rc<opr::Unit>>,
                                 let unit_name = (*unit).formatted_name();
                                 //let opr::Unit{..} = *unit;
                                 view! {
-                                    <ltn::Tr on:click=move |_| {
+                                    <ltn::TableRow on:click=move |_| {
                                         select_unit.set(Some(unit.clone()));
                                     }>
-                                        <ltn::Td> {unit_name} </ltn::Td>
-                                    </ltn::Tr>
+                                        <ltn::TableCell> {unit_name} </ltn::TableCell>
+                                    </ltn::TableRow>
                                 }
                             })
                             .collect_view()
                     }}
-                </ltn::Tbody>
+                </ltn::TableBody>
             </ltn::Table>
         </ltn::TableContainer>
     }
@@ -205,7 +215,7 @@ fn EquipmentList(loadout_list: Vec<Rc<opr::UnitLoadout>>) -> impl IntoView {
     view! {
         <ltn::TableContainer>
             <ltn::Table bordered=true hoverable=true>
-                <ltn::Tbody>
+                <ltn::TableBody>
                     {move || {
                         loadout_list
                             .clone()
@@ -220,7 +230,7 @@ fn EquipmentList(loadout_list: Vec<Rc<opr::UnitLoadout>>) -> impl IntoView {
                             })
                             .collect_view()
                     }}
-                </ltn::Tbody>
+                </ltn::TableBody>
             </ltn::Table>
         </ltn::TableContainer>
     }
@@ -233,24 +243,24 @@ fn EquipmentItem(loadout: Rc<opr::UnitLoadout>) -> impl IntoView {
         let special_rules = equipment.special_rules.clone();
         let opr::Equipment{count, range, attacks, ..} = *equipment;
         view! {
-            <ltn::Tr>
-                <ltn::Td>
+            <ltn::TableRow>
+                <ltn::TableCell>
                     {if count != 1
                         {format!("{}x ", count)} else {"".to_string()}}
                     {name}
-                </ltn::Td>
-                <ltn::Td>
+                </ltn::TableCell>
+                <ltn::TableCell>
                     {if range != 0
                         {format!(r#"{}""#, range )}
                         else {"-".to_string()}}
-                </ltn::Td>
-                <ltn::Td>
+                </ltn::TableCell>
+                <ltn::TableCell>
                     {format!("A{}", attacks)}
-                </ltn::Td>
-                <ltn::Td>
+                </ltn::TableCell>
+                <ltn::TableCell>
                     <SpecialRulesList special_rules={special_rules.clone()} />
-                </ltn::Td>
-            </ltn::Tr>
+                </ltn::TableCell>
+            </ltn::TableRow>
         }
     } else {
         panic!("EquipmentItem must be used on Equipment only");
