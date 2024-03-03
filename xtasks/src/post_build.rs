c33dd5ccd23002c66d7d9524ac1c94043251ef68
Yann Dirson <ydirson@free.fr>
2024-03-03 16:28:49 +0100

    WIP get rid of build.rs
    Instead of a 2-step copy, first done by build.rs, then from there by
    the post_build hook, get the hook to copy directly into trunk staging
    dir.  Ought to be easier to maintain by having all that code in a
    single place.
    
    This moves the dependency on the data crate from a build.rs dep to an
    xtask one.
    
    FIXME:
    - cannot condition this with the feature any more, has to pull the
      dependency on the data crate even when feature is disabled
    - duplicates opr-rs rev in 2 Cargo.toml files
    
    See https://github.com/trunk-rs/trunk/issues/723

--- xtasks/src/post_build.rs
+++ xtasks/src/post_build.rs
@@ -3,27 +3,7 @@ pub fn main() {
         .expect("TRUNK_STAGING_DIR should be set").into();
     let data_dir = staging_dir.join("data");
 
-    // copy "data" into "dist/" only if the feature pulled "data" first
-    if fs::metadata("data").is_ok() {
-        if let Err(e) = recursive_copy("data", &data_dir) {
-            eprintln!("error: {e:?}");
-        }
+    if let Err(e) = opr_test_data::import_data(&data_dir) {
+        eprintln!("error: {e:?}");
     }
-}
-
-use std::path::Path;
-use std::{io, fs};
-
-fn recursive_copy(src: impl AsRef<Path>, dst: impl AsRef<Path>) -> io::Result<()> {
-    fs::create_dir_all(&dst)?;
-    for entry in fs::read_dir(src)? {
-        let entry = entry?;
-        let ty = entry.file_type()?;
-        if ty.is_dir() {
-            recursive_copy(entry.path(), dst.as_ref().join(entry.file_name()))?;
-        } else {
-            fs::copy(entry.path(), dst.as_ref().join(entry.file_name()))?;
-        }
-    }
-    Ok(())
 }
