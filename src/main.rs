f7c85563b598c7c502a4083b82fcd02f67d08fab
Yann Dirson <ydirson@free.fr>
2024-03-05 22:25:46 +0100

    ArmyList: close outdated drawer when a new Army gets loaded
    Note this cannot be directly tested with just this code, it requires
    adding links in the ArmyView, like:
    
        <a href="./?armies=Mlwpoh1AGLC2,p2KIbSBOYpSB">change left</a>
        <a href="./?armies=Rrlct39EGuct,Mlwpoh1AGLC2">change right</a>

--- src/main.rs
+++ src/main.rs
@@ -259,6 +259,11 @@ fn ArmyList(army: Army,
                         let name = Rc::clone(name);
                         let units = units.clone();
 
+                        // since we are recreating an ArmyList, the Army must have changed
+                        // (or this is an over-refresh bug), drawer is outdated so close it
+                        let shown = use_context::<DrawerControl>().unwrap().shown;
+                        shown.set(false);
+
                         let game_system = opr::GameSystem::from_str(game_system);
                         let check_inconsistency = move || {
                             match game_system.clone() {
